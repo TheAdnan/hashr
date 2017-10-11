f5e495dabbfaadbfcf552400b71f62daf9ccfd5b
Jay Shastrakar <jayshastrakar97@gmail.com>
2017-10-11 23:29:50 +0530

    changes in UI
    updated grid properties and updated form UI

--- index.php
+++ index.php
@@ -1,42 +1,33 @@
-<?php 
+<?php
 	require_once("function.php");
  ?>
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Hashr</title>
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-	<style type="text/css">
-		html{
-			font-family: Orbitron;
-		}
-		.hashes {
-			display: grid;
-			grid-template-columns: repeat(2, 1fr);
-			grid-gap: 10px;
-			grid-auto-columns: minmax(20%, 30%);
-		}
-	</style>
 	<link href="https://fonts.googleapis.com/css?family=Nunito|Orbitron" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<form method="post">
-		<label>Input string: </label>
-		<input type="text" name="input">
-		<input type="submit" name="submit" value="Hash!">
-	</form>
-	<br>
-	<div class="hashes">
-		<?php 
+
+<div class="container">
+	<header class="headform card">
+		<form method="post">
+			<label class="form-label">Input string: </label>
+			<input type="text" name="input" class="form-input" placeholder="Enter your input String">
+			<input type="submit" name="submit" value="hash!">
+		</form>
+	</header>
+	<?php
 			foreach (hash_algos() as $i => $algo) { ?>
-				<div class="hash_algo">
-					<p class="algo"><b><?= $algo; ?></b></p>
-					<p>Hash: <span style="font-family: Nunito"><?= $hashes[$i]; ?></span></p>
-				</div>
+				<article class="item-card card">
+					<span class="algo_name"><?= $algo; ?></span>
+					<p>Hash: <span class="text_style"><?= $hashes[$i]; ?></span></p>
+				</article>
 		<?php	}
 		 ?>
-	</div>
+</div>
 </body>
 </html>
-
-
