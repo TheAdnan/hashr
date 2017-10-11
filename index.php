3f2daf1283a6ba885017f68c589237e07bdccd6d
Ivan Djurdjevac <djurdjevac@gmail.com>
2017-10-11 09:45:37 +0000

    Caclulating hash time

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
 				<div class="hash_algo">
 					<p class="algo"><b><?= $algo; ?></b></p>
 					<p>Hash: <span style="font-family: Nunito"><?= $hashes[$i]; ?></span></p>
+                    <small>Execution time: <?= number_format($hashTime[$algo] * 1000000000, 0); ?> nanoseconds</small>
 				</div>
 		<?php	}
 		 ?>
